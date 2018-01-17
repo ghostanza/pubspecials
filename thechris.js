4607b3d2a12f0533eba197256c56f5338df6844d
ghostanza <friedcaligari@gmail.com>
2018-01-16 20:14:45 -0500

    adding beer listings

--- thechris.js
+++ thechris.js
@@ -1,71 +1,112 @@
 const http = require('http');
 const args = process.argv.slice(2);
 let flags = [];
+let toShow = [];
+let lookup= {
+  'flagsToNames':{
+    'a': 'appetizer',
+    'p': 'big plates',
+    's': 'soup',
+    'g': 'burger',
+    'b': 'beer specials',
+    'f': 'catch',
+    'c': 'from the bar'
+  },
+  'namesToHeadings':{
+    'appetizer': "APPETIZERS",
+    'big plates': "BIG PLATES",
+    'soup': "SOUP",
+    'burger': "BURGER",
+    'catch': "CATCH OF THE DAY",
+    'from the bar': "COCKTAILS",
+    'beer specials': "BEER SPECIALS"
+  }
+}
 let markup = " ";
 for (i in args) {
   args[i][0]=='-' && flags.push(...args[i].slice(1).split(''))
 }
-http.get('http://christopherscambridge.com.s122913.gridserver.com/menus/194-2', (res)=>{
-  res.on('data', (c)=>{
-    markup += `${c}`;
-  }).on('end', () => {
-    if(markup.length){
-        let finalOutput = '';
-        markup = markup.split(/[\n\t]/).join('');
-        let findU=/updated ([^<&-]*)?/g;
-        let u = findU.exec(markup)[1];
-        console.log(`++++++++++++++++\nUPDATED ON ${u}\n++++++++++++++++`);
-        let apps = findSpecialItem('appetizer', markup);
-        let plates = findSpecialItem('big plates', markup);
-        let soup = findSpecialItem('soup', markup);
-        let burger = findSpecialItem('burger', markup);
-        let fish = findSpecialItem('catch', markup);
-        let cocktail = findSpecialItem('from the bar', markup);
+if(flags.includes('h')){
+  console.log(`
+      Need Help?
+      Do not use any flags if you want to see all of the specials.
 
-        if(flags.length){
-          if(flags.indexOf('a') >= 0){
-            finalOutput+=`\nAPPETIZERS:\n==========\n${apps}\n`;
-          }
-          if(flags.indexOf('b') >= 0){
-            finalOutput+=`\nBURGER:\n==========\n${burger}\n`;
-          }
-          if(flags.indexOf('p') >= 0){
-            finalOutput+=`\nBIG PLATES:\n==========\n${plates}\n`;
-          }
-          if(flags.indexOf('s') >= 0){
-            finalOutput+=`\nSOUP:\n==========\n${soup}\n`;
-          }
-          if(flags.indexOf('f') >= 0){
-            finalOutput+=`\nCATCH OF THE DAY:\n==========\n${fish}\n`;
-          }
-          if(flags.indexOf('c') >= 0){
-            finalOutput+=`\nCOCKTAILS:\n==========\n${cocktail}\n`;
-          }
-        }
-        else{
-          finalOutput += `\nAPPETIZERS:\n==========\n${apps}\n\nBURGER:\n==========\n${burger}\n\nBIG PLATES:\n==========\n${plates}\n\nSOUP:\n==========\n${soup}\n\nCATCH OF THE DAY:\n==========\n${fish}\n\nCOCKTAIL:\n==========\n${cocktail}\n`;
-        }
-        console.log(finalOutput);
-    }
+      FLAGS (can be combined):
+      [-a]: appetizers
+      [-b]: beer
+      [-c]: cocktails
+      [-f]: catch of the day
+      [-g]: burger
+      [-p]: big plates
+      [-s]: soup
 
+      HELP:
+      [-h]: Will override any other flags and display this help menu
+
+      EXAMPLES:
+        node thechris.js -ac (will return appetizers and cocktails)
+        node thechris.js -p -s (big plates / soup)
+    `);
+}
+else{
+  http.get('http://christopherscambridge.com.s122913.gridserver.com/menus/194-2', (res)=>{
+    res.on('data', (c)=>{
+      markup += `${c}`;
+    }).on('end', () => {
+      if(markup.length){
+          let finalOutput = '';
+          markup = markup.split(/[\n\t]/).join('');
+          let findU=/updated ([^<&-]*)?/g;
+          let u = findU.exec(markup)[1];
+          console.log(`+++++++++++++++++++++++\nUPDATED ON ${u}\n+++++++++++++++++++++++`);
+          if(flags.length){
+            for(k in lookup.flagsToNames){
+              if(flags.includes(k)){
+                toShow.push(lookup.flagsToNames[k]);
+              }
+            }
+          } else{
+            toShow.push(...Object.keys(lookup.namesToHeadings));
+          }
+          let f = findItemInfo(toShow, markup);
+          console.log(`${f}\n`);
+      }
+
+    });
+  }).on('error', (e)=>{
+    console.log(`ERROR: ${e.message}`);
   });
-}).on('error', (e)=>{
-  console.log(`ERROR: ${e.message}`);
-});
 
-function findSpecialItem(name, findIn){
-  let reg = new RegExp(`${name}.*?</h6>(.*?)<h`,'gi');
-  let matches = reg.exec(findIn),
-      m = '';
-  if(matches.length > 1){
-    let t = matches[1].split(/<\/p>/).slice(0,-1);
-    for(j in t){
-      t[j] = t[j].replace(/<br.*?\/>/g, '<br/>\n');
-      t[j] = t[j].replace(/<.*?>/g, '');
-      t[j] = t[j].replace(/&amp;/g, 'and');
-      t[j] = t[j].replace(/(\d+\.\d+)/g, "\n- $1");
+}
+
+function findItemInfo(names, findIn){
+  let final = '';
+  for(n in names){
+    let reg = new RegExp(`${names[n]}.*?</h6>(.*?)<h6`,'gi');
+    let matches = reg.exec(findIn),
+        m = `\n\n${lookup.namesToHeadings[names[n]]}\n====================\n`;
+    if(matches.length > 1){
+      let t = matches[1].split(/<\/p>/).slice(0,-1);
+      for(j in t){
+        if(names[n] == 'beer specials'){
+          if(t[j].match(/<h1>/g)){
+            let findHeading = new RegExp('<h1>.*?<strong>(.*?)</', 'g')
+            let heading = findHeading.exec(t[j]);
+            if(heading.length > 1){
+              t[j] = `--------------------\n ${heading[1].toUpperCase()} \n--------------------\n${t[j]}`;
+              t[j] = t[j].replace(/(<h1>.*?<\/h1>)/g, '');
+            }
+          }
+        }
+        t[j] = t[j].replace(/<br.*?\/>/g, '<br/>\n');
+        t[j] = t[j].replace(/<.*?>/g, '');
+        t[j] = t[j].replace(/&amp;/g, 'and');
+        t[j] = t[j].replace(/&#8217;/g, '\'');
+        t[j] = t[j].replace(/(\d+\.\d+)/g, "\n- $1\n");
+      }
+      m += t.join('\n\n');
     }
-    m = t.join('\n\n');
+    final+=m;
   }
-  return m;
+  return final;
 }
